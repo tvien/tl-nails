26e8dfac7465f97fbe9e441083d808ccca105a4d
tvien <tonyxvien@gmail.com>
2021-08-01 21:18:31 -0400

    updated ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
 			options: {
 				// You can add multiple tracking ids and a pageview event will be fired for all of them.
 				trackingIds: [
-					'G-PZ9JPE5ME1', // Google Analytics / GA
+					'UA-200634681-1', // Google Analytics / GA
 					// 'AW-CONVERSION_ID', // Google Ads / Adwords / AW
 					// 'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
 				],
