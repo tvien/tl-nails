1d414f6371536e007df994e966b1bc89a80fb8a2
tvien <34506545+tvien@users.noreply.github.com>
2025-01-21 20:01:33 -0500

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -76,7 +76,7 @@ const SEO = ({ title, description, image, article }) => {
 			<link rel='icon' href={favicon} />
 			<!-- Hotjar Tracking Code for https://www.tlnailstampa.com/ -->
 			<script>
-			    (function(h,o,t,j,a,r){
+				{`(function(h,o,t,j,a,r){
 			        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
 			        h._hjSettings={hjid:5276222,hjsv:6};
 			        a=o.getElementsByTagName('head')[0];
@@ -84,7 +84,8 @@ const SEO = ({ title, description, image, article }) => {
 			        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
 			        a.appendChild(r);
 			    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-			</script>
+			`}
+			       </script>
 			{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
 			<script
 				async
