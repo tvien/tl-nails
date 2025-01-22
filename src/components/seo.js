1f143e9e91b1c065c73fec9bd582cfea8cd895e7
tvien <34506545+tvien@users.noreply.github.com>
2025-01-21 19:56:51 -0500

    Update seo.js
    Added Hotjar

--- src/components/seo.js
+++ src/components/seo.js
@@ -74,7 +74,17 @@ const SEO = ({ title, description, image, article }) => {
 			{seo.image && <meta name='twitter:image' content={seo.image} />}
 
 			<link rel='icon' href={favicon} />
-
+			<!-- Hotjar Tracking Code for https://www.tlnailstampa.com/ -->
+			<script>
+			    (function(h,o,t,j,a,r){
+			        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+			        h._hjSettings={hjid:5276222,hjsv:6};
+			        a=o.getElementsByTagName('head')[0];
+			        r=o.createElement('script');r.async=1;
+			        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+			        a.appendChild(r);
+			    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+			</script>
 			{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
 			<script
 				async
